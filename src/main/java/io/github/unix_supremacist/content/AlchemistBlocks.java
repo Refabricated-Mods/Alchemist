f1ce517dbdc8b282371d7d456f0d25cf793e7e3d
Unix <unix-supremacist@users.noreply.github.com>
2023-12-07 20:04:47 -0600

    alabli blockproperties

--- src/main/java/io/github/unix_supremacist/content/AlchemistBlocks.java
+++ src/main/java/io/github/unix_supremacist/content/AlchemistBlocks.java
@@ -1,6 +1,7 @@
 package io.github.unix_supremacist.content;
 
 import io.github.unix_supremacist.Alchemist;
+import io.github.unix_supremacist.alabli.Alabli;
 import io.github.unix_supremacist.block.TransmutionCircleBlock;
 import lombok.Getter;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -10,7 +11,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
 public enum AlchemistBlocks {
-    transmutation_circle(new TransmutionCircleBlock(FabricBlockSettings.create()));
+    transmutation_circle(new TransmutionCircleBlock(Alabli.properties));
     @Getter Block block;
 
     AlchemistBlocks(Block block){
